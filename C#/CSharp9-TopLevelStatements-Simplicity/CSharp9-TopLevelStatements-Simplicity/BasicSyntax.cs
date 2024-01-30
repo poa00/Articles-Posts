8c0323971b020972588832d3538ce31149e4c128
Ziggy Rafiq <ziggyrafiq@hotmail.com>
2024-01-30 19:02:14 +0000

    update

--- C#/CSharp9-TopLevelStatements-Simplicity/CSharp9-TopLevelStatements-Simplicity/BasicSyntax.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CSharp9_TopLevelStatements_Simplicity;
-public static class BasicSyntax
-{
-    public static void SimpleExample()
-    {
-        Console.WriteLine("Hello, C# 9 Top-Level Statements!");
-    }
-}
--- C#/CSharp9-TopLevelStatements-Simplicity/CSharp9-TopLevelStatements-Simplicity/CodeOrganisation/Calculator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CSharp9_TopLevelStatements_Simplicity.CodeOrganisation
-{
-    // Define a class for a more organized structure
-    public class Calculator
-    {
-        public int Add(int a, int b) => a + b;
-    }
-
-}
